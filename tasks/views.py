d70f4593937e5677dae23dbb416b65d12c97fd60
suvorovjr <pglavan1998@mail.ru>
2024-07-09 10:05:38 +0300

    add TaskAsCompletedAPIView

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .paginators import TaskPaginator
 from .serializers import TaskSerializer
 from .models import Task
@@ -26,3 +27,14 @@ class TaskUpdateAPIView(generics.UpdateAPIView):
 
 class TaskDestroyAPIView(generics.DestroyAPIView):
     queryset = Task.objects.all()
+
+
+class TaskAsCompletedAPIView(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def update(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.status = 'completed'
+        task.save()
+        return Response({'status': 'task marked as completed'}, status=status.HTTP_200_OK)
