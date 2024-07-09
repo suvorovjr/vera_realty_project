3104ec93d9cb8e86bf62e5843c826e572a8dacc2
suvorovjr <pglavan1998@mail.ru>
2024-07-09 10:54:13 +0300

    add changes in urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from .apps import TasksConfig
-from .views import TaskCreateAPIView, TaskListAPIView, TaskRetrieveAPIView, TaskUpdateAPIView, TaskDestroyAPIView
+from .views import TaskCreateAPIView, TaskListAPIView, TaskRetrieveAPIView, TaskUpdateAPIView, TaskDestroyAPIView, \
+    TaskAsCompletedAPIView, CommentCreateAPIView
 
 app_name = TasksConfig.name
 
 urlpatterns = [
     path('create/', TaskCreateAPIView.as_view(), name='create'),
     path('list/', TaskListAPIView.as_view(), name='list'),
-    path('view/<int:pk>/', TaskRetrieveAPIView.as_view(), name='view'),
-    path('update/<int:pk>/', TaskUpdateAPIView.as_view(), name='update'),
-    path('delete/<int:pk>/', TaskDestroyAPIView.as_view(), name='delete'),
+    path('view/<uuid:pk>/', TaskRetrieveAPIView.as_view(), name='view'),
+    path('update/<uuid:pk>/', TaskUpdateAPIView.as_view(), name='update'),
+    path('delete/<uuid:pk>/', TaskDestroyAPIView.as_view(), name='delete'),
+    path('change-status/<uuid:pk>/', TaskAsCompletedAPIView.as_view(), name='change-status'),
+    path('add-comment/', CommentCreateAPIView.as_view(), name='add-comment'),
 ]
